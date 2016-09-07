883e409811c999dc810c9da21a988fabcd9d21b1
Long Pham <longpham@Ksquares-MacBook-Pro-2.local>
2016-09-07 06:02:31 -0500

    first exercise chapter 8

---
+++ JS.Ninja/chapter8/JSChapter8.js
@@ -0,0 +1,26 @@
+// var x = "These are the cars I like";
+// var toStart = setInterval(peak, 1000);
+// var cars = {
+//
+//  1: "pathfinder is a good family car",
+//  2: "Murano is a good suv",
+//  3: "Altima is a good vehicle for long distance to save gas",
+//  4: "BMW just like the car",
+//  5: "Lamborgini is a very fast and expensive car",
+//  6: "Audi very smooth to drive",
+//
+// };
+//
+// min = Math.ceil(6);
+// max = Math.floor(1);
+// test = 0;
+// function peak(){
+//  test  = Math.floor(Math.random() * (max-min))+min;
+//  console.log(cars[test]);
+// }
+// function toStop() {
+// clearInterval(cars);
+// }
+//
+// console.log(x)
+// console.log(toStart);
