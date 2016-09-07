db76592c1afe5c75b2f284686267c144505557d9
Long Pham <longpham@Ksquares-MacBook-Pro-2.local>
2016-09-07 06:30:39 -0500

    3rd update  exercise chapter 8

--- JS.Ninja/chapter8/JSChapter8.js
+++ JS.Ninja/chapter8/JSChapter8.js
@@ -24,3 +24,62 @@
 //
 // console.log(x)
 // console.log(toStart);
+
+
+// var timers = {
+//      timerID: 0,
+//      timers: [],
+//      time: 1,
+//      add: function(fn) {
+//          this.timers.push(fn)
+//  },
+//  start: function() {
+//    if(this.timerID) return;
+//    (function runNext() {
+//      if (timers.timers.length > 0) {
+//        for(var i = 0; i < timers.timers.length; i++){
+//          if((timers.time%timers.timers[i].interval === 0) || (!timers.timers[i])){
+//            if (timers.timers[i].fun() === false){
+//              timers.timers.splice(i,1);
+//              i--;
+//            }
+//          }
+//        }
+//        timers.time++;
+//        timers.timersID = setTimeout(runNext,15000);
+//      }
+//    })();
+//  },
+//  stop: function() {
+//      clearTimeout(this.timerID);
+//      this.timerID = 0;
+//  }
+//  };
+//
+//  //set time to 30 seconds (2 * 15 = 30)
+//  timers.add({
+//      fun:
+//          function A() {
+//              console.log("ready");
+//          },
+//          interval: 2
+//  });
+//
+//  //set time to 60 seconds (4 * 15 = 60)
+//  timers.add({
+//      fun:
+//          function B() {
+//              console.log("aim");
+//          },
+//          interval: 4
+//  });
+//  //set time to 75 seconds (5 * 15 = 75)
+//  timers.add({
+//      fun:
+//          function C() {
+//              console.log("fire");
+//      },
+//    interval: 5
+//  });
+//
+//  timers.start();
