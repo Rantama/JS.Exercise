9f62120e83337760edfd98115445c4bbc0fc19e5
Long Pham <longpham@Ksquares-MacBook-Pro-2.local>
2016-09-08 05:22:09 -0500

    first push for chapter12

---
+++ JS.Ninja/chapter12/JSChapter12.js
@@ -0,0 +1,13 @@
+<div id = 'a' class = 'square' style = 'display:inlineblock' val = 'something important'></div>
+
+<script type = "text/>javascript">
+  window.onload = function() {
+    var el = document.getElementById("a"),
+    id = document.getElementById("id"),
+    class1 = document.getElementById('class'),
+    style = document.getElementById("style"),
+    val = document.getElementById('val');
+
+
+  }
+</script>
