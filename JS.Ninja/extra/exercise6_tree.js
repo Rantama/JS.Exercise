17a0dbd8b4d26f703e7a74058a55550037047cbe
Long Pham <longpham@Ksquares-MacBook-Pro-2.local>
2016-09-21 08:54:35 -0500

    update extra exercise 6 tree

---
+++ JS.Ninja/extra/exercise6_tree.js
@@ -0,0 +1,44 @@
+//exercise 6 tree
+
+
+var A = {
+		A: {
+			a: {
+				aa: 'aa'
+			},
+			b: {
+				ba: 'ba',
+				bb: 'bb'
+			},
+			c: {
+				ca: 'ca',
+				cb: {
+					cba: 'cba'
+				}
+
+			}
+		}
+};
+var numb = 0;
+function buildTree(tree) {
+	//var numb = 0; // did not work, moving var outside of function
+
+	for( var k in tree) {
+
+		if(typeof tree[k] == "object" && tree[k] !== null){
+
+			console.log(" " . repeat(numb) + (k));
+			numb++;
+			buildTree(tree[k]);
+
+		} else {
+
+			// console.log(tree[k])  // did not move all of the children to the next point
+			console.log(" " . repeat(numb) + [k]);  //moved all the children to the correct spaces
+		}
+
+
+	}
+	numb--;
+}
+buildTree(A);
