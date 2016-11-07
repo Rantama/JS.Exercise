cada377701c500174f3a7eacf10dbc159b49b85f
Long Pham <longpham@Ksquares-MacBook-Pro-2.local>
2016-11-07 04:49:52 -0600

    updated main.js

--- JS.Ninja/final/app/main.js
+++ JS.Ninja/final/app/main.js
@@ -1,3 +1,9 @@
+var removeIMG = '<img src="images/deletebutton.jpeg" class="smalls">';
+
+var saveIMG = '<img src="images/saveicon.png" class="smalls">'
+
+var editIMG = '<img src="images/editbutton.png" class="smalls">'
+
 document.getElementById('add_button').addEventListener('click',function() {
 	var value = document.getElementById('title_val').value;
 	if (value) addItemTodo(value);
@@ -6,7 +12,33 @@ document.getElementById('add_button').addEventListener('click',function() {
 });
 
 function addItemTodo(text) {
-	
+
+	var list = document.getElementById('todo');
+
+	var title_val = document.createElement('li');
+	item.innerHTML = text;
+
+	var buttons = document.createElement('div');
+	buttons.classList.add('buttons');
+
+	var remove = document.createElement('button');
+	remove.classList.add('remove');
+	remove.innerHTML = removeIMG;
+
+	var edit = document.createElement('button');
+	edit.classList.add('edit');
+	edit.innerHTML = editIMG;
+
+	var save = document.createElement('button');
+	save.classList.add('save');
+	save.innerHTML = saveIMG;
+
+	buttons.appendChild(remove);
+	buttons.appendChild(save);
+	buttons.appendChild(edit);
+	title_val.appendChild(buttons);
+
+	list.appendChild(title_val);
 }
 // require([],
 // function(){
